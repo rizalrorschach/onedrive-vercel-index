2989810cd7f110ca1bca549e6a30ba1c41639310
rizalrorschach <150176061+rizalrorschach@users.noreply.github.com>
2023-11-26 18:47:20 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -52,8 +52,7 @@ module.exports = {
   // you provide. See the documentation for details.
  links: [
     {
-      name: 'GitHub',
-      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
+      
     },
   ],
 
